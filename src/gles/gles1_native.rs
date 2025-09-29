1f60153034ebd2adbe07d3b2e4bc58a15c7ebefe
Vrey667 <158614150+totvrey5@users.noreply.github.com>
2025-09-29 03:25:34 +0200

    Update gles1_native.rs

--- src/gles/gles1_native.rs
+++ src/gles/gles1_native.rs
@@ -118,9 +118,6 @@ impl GLES for GLES1Native<'_> {
     unsafe fn DisableClientState(&mut self, array: GLenum) {
         gles11::DisableClientState(array)
     }
-    unsafe fn GetFixedv(&mut self, pname: GLenum, params: *mut GLboolean) {
-        gles11::GetFixedv(pname, params)
-    }
     unsafe fn GetBooleanv(&mut self, pname: GLenum, params: *mut GLboolean) {
         gles11::GetBooleanv(pname, params)
     }
