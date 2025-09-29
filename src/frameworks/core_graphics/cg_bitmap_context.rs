f7186cfc6209549ea2e128decf7a9eb575a60be4
Vrey667 <158614150+totvrey5@users.noreply.github.com>
2025-09-29 21:12:19 +0200

    Update cg_bitmap_context.rs

--- src/frameworks/core_graphics/cg_bitmap_context.rs
+++ src/frameworks/core_graphics/cg_bitmap_context.rs
@@ -9,7 +9,7 @@ use super::cg_affine_transform::{CGAffineTransform, CGAffineTransformIdentity};
 use super::cg_color_space::{
     kCGColorSpaceGenericGray, kCGColorSpaceGenericRGB, CGColorSpaceHostObject, CGColorSpaceRef,
 };
-use super::cg_context::{CGBlendMode, CGContextHostObject, CGContextRef, CGContextSubclass};
+use super::cg_context::{CGBlendMode, CGContextHostObject, CGContextRef, CGContextState, CGContextSubclass};
 use super::cg_image::{
     self, kCGBitmapAlphaInfoMask, kCGBitmapByteOrderMask, kCGImageAlphaFirst, kCGImageAlphaLast,
     kCGImageAlphaNone, kCGImageAlphaNoneSkipFirst, kCGImageAlphaNoneSkipLast, kCGImageAlphaOnly,
