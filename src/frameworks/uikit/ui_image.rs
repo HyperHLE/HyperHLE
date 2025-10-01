9a5944c5456adc86d03ef9f6ab623338eb59e47e
Vrey667 <158614150+totvrey5@users.noreply.github.com>
2025-10-01 08:27:59 +0200

    Update ui_image.rs

--- src/frameworks/uikit/ui_image.rs
+++ src/frameworks/uikit/ui_image.rs
@@ -132,7 +132,9 @@ pub const CLASSES: ClassExports = objc_classes! {
 - (id)initWithData:(id)data { // NSData*
     let slice = ns_data::to_rust_slice(env, data);
     // TODO: refactor common parts
-    let image = Image::from_bytes(slice).unwrap();
+    let image = Image::from_bytes(slice).ok()?;        // decode or nil
+    let uiimage = UIImage::from_image(env, image);     // wrap into ObjC object
+    uiimage
     let cg_image = cg_image::from_image(env, image);
     env.objc.borrow_mut::<UIImageHostObject>(this).cg_image = cg_image;
     this
