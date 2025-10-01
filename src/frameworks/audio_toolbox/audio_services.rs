b623585babd9ff83df54f16d5a8cf56d4552e657
Vrey667 <158614150+totvrey5@users.noreply.github.com>
2025-10-01 13:05:54 +0200

    Update audio_services.rs

--- src/frameworks/audio_toolbox/audio_services.rs
+++ src/frameworks/audio_toolbox/audio_services.rs
@@ -10,6 +10,7 @@ use crate::frameworks::carbon_core::OSStatus;
 use crate::frameworks::core_audio_types::fourcc;
 use crate::mem::{MutPtr, MutVoidPtr};
 use crate::Environment;
+use crate::frameworks::core_foundation::cf_url::CFURLRef;
 
 /// Usually a FourCC.
 type AudioServicesPropertyID = u32;
@@ -31,17 +32,32 @@ fn AudioServicesGetProperty(
     if in_property_id == 0xfff {
         kAudioServicesUnsupportedPropertyError
     } else {
-        unimplemented!("AudioServicesGetProperty is not implemented for property ID: {}", in_property_id);
+        unimplemented!();
     }
 }
 
 fn AudioServicesPlaySystemSound(_env: &mut Environment, in_system_sound_id: SystemSoundID) {
-    assert_eq!(in_system_sound_id, kSystemSoundID_Vibrate);
+    // assert_eq!(in_system_sound_id, kSystemSoundID_Vibrate);
     log!("TODO: vibration (AudioServicesPlaySystemSound)");
     // TODO: implement other system sounds
+}
+
+fn AudioServicesDisposeSystemSoundID(_env: &mut Environment, in_system_sound_id: SystemSoundID) {
+    // assert_eq!(in_system_sound_id, kSystemSoundID_Vibrate);
+    log_dbg!("TODO: vibration (AudioServicesDisposeSystemSoundID)");
+    // TODO: implement other system sounds
+}
+fn AudioServicesCreateSystemSoundID(
+    env: &mut Environment,
+    in_file_url: CFURLRef,
+    in_system_sound_id: SystemSoundID
+) -> OSStatus {
+    -1
 }
 
 pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(AudioServicesGetProperty(_, _, _, _, _)),
     export_c_func!(AudioServicesPlaySystemSound(_)),
+    export_c_func!(AudioServicesDisposeSystemSoundID(_)),
+    export_c_func!(AudioServicesCreateSystemSoundID(_, _)),
 ];
