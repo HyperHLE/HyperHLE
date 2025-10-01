9395895ab238bd3180dbb86b92ff60125161bd4d
Vrey667 <158614150+totvrey5@users.noreply.github.com>
2025-10-01 09:59:19 +0200

    Update ns_locale.rs

--- src/frameworks/foundation/ns_locale.rs
+++ src/frameworks/foundation/ns_locale.rs
@@ -120,6 +120,10 @@ pub const CLASSES: ClassExports = objc_classes! {
     msg![env; this init]
 }
 
++ (id)rangeOfString:(NSUInteger)string {
+    msg![env; this init]
+}
+
 + (id)autoupdatingCurrentLocale {
     nil
 }
