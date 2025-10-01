b405f21b7be8e99727f960e1f2d1d70c22e4c02e
Vrey667 <158614150+totvrey5@users.noreply.github.com>
2025-10-01 12:53:38 +0200

    Update math.rs

--- src/libc/math.rs
+++ src/libc/math.rs
@@ -610,6 +610,9 @@ fn CGPathRelease(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
 fn ExtAudioFileRead(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
     arg1.min(arg2)
 }
+fn AudioQueueNewInput(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
+    arg1.min(arg2)
+}
 
 pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(abs(_)),
@@ -738,4 +741,5 @@ pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(CGPathCreateMutable(_, _)),
     export_c_func!(CGPathRelease(_, _)),
     export_c_func!(ExtAudioFileRead(_, _)),
+    export_c_func!(AudioQueueNewInput(_, _)),
 ];
