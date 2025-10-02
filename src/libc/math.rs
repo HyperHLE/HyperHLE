4a49e52c2ef6e06fd33ee17b2cdb90539b0f9061
Vrey667 <158614150+totvrey5@users.noreply.github.com>
2025-10-02 18:27:41 +0200

    Update math.rs

--- src/libc/math.rs
+++ src/libc/math.rs
@@ -751,6 +751,9 @@ fn pthread_cancel(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
 fn statvfs(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
     arg1.min(arg2)
 }
+fn mprotect(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
+    arg1.min(arg2)
+}
 
 pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(abs(_)),
@@ -926,4 +929,5 @@ pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(pthread_exit(_, _)),
     export_c_func!(pthread_cancel(_, _)),
     export_c_func!(statvfs(_, _)),
+    export_c_func!(mprotect(_, _)),
 ];
