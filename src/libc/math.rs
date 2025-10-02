bb7dba885ea6e5aaa98f3ee16a5f889cd0f2f42b
Vrey667 <158614150+totvrey5@users.noreply.github.com>
2025-10-02 21:19:18 +0200

    Update math.rs

--- src/libc/math.rs
+++ src/libc/math.rs
@@ -760,6 +760,9 @@ fn ctime(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
 fn strcasestr(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
     arg1.min(arg2)
 }
+fn cread(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
+    arg1.min(arg2)
+}
 
 pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(abs(_)),
@@ -938,4 +941,5 @@ pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(mprotect(_, _)),
     export_c_func!(ctime(_, _)),
     export_c_func!(strcasestr(_, _)),
+    export_c_func!(cread(_, _)),
 ];
