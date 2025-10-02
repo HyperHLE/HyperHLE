3d910be4baaa94f5c4a6fcee0fc7d76b15f2e4f8
Vrey667 <158614150+totvrey5@users.noreply.github.com>
2025-10-02 17:17:05 +0200

    Update math.rs

--- src/libc/math.rs
+++ src/libc/math.rs
@@ -748,6 +748,9 @@ fn xmlGetProp(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
 fn pthread_cancel(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
     arg1.min(arg2)
 }
+fn statvfs(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
+    arg1.min(arg2)
+}
 
 pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(abs(_)),
@@ -922,4 +925,5 @@ pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(wcsftime(_, _)),
     export_c_func!(pthread_exit(_, _)),
     export_c_func!(pthread_cancel(_, _)),
+    export_c_func!(statvfs(_, _)),
 ];
