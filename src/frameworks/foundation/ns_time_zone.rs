4ed87afcd488be74b8f08cf90374e674dfbf3ec5
Vrey667 <158614150+totvrey5@users.noreply.github.com>
2025-10-03 16:38:45 +0200

    Update ns_time_zone.rs

--- src/frameworks/foundation/ns_time_zone.rs
+++ src/frameworks/foundation/ns_time_zone.rs
@@ -40,10 +40,22 @@ pub const CLASSES: ClassExports = objc_classes! {
     msg![env; this timeZoneWithName:tz_name]
 }
 
++ (id)systemTimeZone {
+    nil
+}
+
 - (())dealloc {
     let tz_name = env.objc.borrow_mut::<NSTimeZoneHostObject>(this).time_zone;
     release(env, tz_name);
     env.objc.dealloc_object(this, &mut env.mem)
+}
+
+- (id)name {
+    nil
+}
+
+- (id)daylightSavingTimeOffset {
+    nil
 }
 
 - (id)initWithName:(id)tz_name { // NSString *
@@ -51,10 +63,6 @@ pub const CLASSES: ClassExports = objc_classes! {
     retain(env, tz_name);
     env.objc.borrow_mut::<NSTimeZoneHostObject>(this).time_zone = tz_name;
     this
-}
-
-- (id)name {
-    nil
 }
 
 - (NSInteger)secondsFromGMT {
