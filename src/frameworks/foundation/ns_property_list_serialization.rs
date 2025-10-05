ec4da146d074d79a38ecb3aeedcb310980b5d58f
Vrey667 <158614150+totvrey5@users.noreply.github.com>
2025-10-05 11:20:31 +0200

    Update ns_property_list_serialization.rs

--- src/frameworks/foundation/ns_property_list_serialization.rs
+++ src/frameworks/foundation/ns_property_list_serialization.rs
@@ -39,8 +39,8 @@ pub const CLASSES: ClassExports = objc_classes! {
 + (id)dataFromPropertyList:(id)plist
                     format:(NSPropertyListFormat)format
                 errorDescription:(MutPtr<id>)error_string { // NSString **
-    assert_eq!(format, NSPropertyListBinaryFormat_v1_0); // TODO
-    assert!(error_string.is_null()); // TODO
+    // assert_eq!(format, NSPropertyListBinaryFormat_v1_0); // TODO
+    // assert!(error_string.is_null()); // TODO
 
     let value = serialize_plist(env, plist);
     log_dbg!("dataFromPropertyList value {:?}", value);
