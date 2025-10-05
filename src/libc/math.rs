ba87f8417f7b0b921cb8fa0667635185fa6d8550
Vrey667 <158614150+totvrey5@users.noreply.github.com>
2025-10-05 10:07:49 +0200

    Update math.rs

--- src/libc/math.rs
+++ src/libc/math.rs
@@ -877,6 +877,9 @@ fn sysconf(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
 fn abort(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
     arg1.min(arg2)
 }
+fn madvise(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
+    arg1.min(arg2)
+}
 
 pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(abs(_)),
@@ -1094,4 +1097,5 @@ pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(AudioFileStreamGetProperty(_, _)),
     export_c_func!(sysconf(_, _)),
     export_c_func!(abort(_, _)),
+    export_c_func!(madvise(_, _)),
 ];
