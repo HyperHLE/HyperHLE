1ba4b3fb82ed907eff87b9c069af5184e78aea43
makaherpasswordlostmedia <emssaoig1@gmail.com>
2025-11-14 20:17:24 +0200

    Update core_animation.rs

--- src/frameworks/core_animation.rs
+++ src/frameworks/core_animation.rs
@@ -19,5 +19,6 @@ pub use composition::recomposite_if_necessary;
 
 #[derive(Default)]
 pub struct State {
+    ca_media_timing_function: ca_media_timing_function::State,
     composition: composition::State,
 }
