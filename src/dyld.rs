abf4d68afa418258a7dcdba7a1a2c04f44da641a
whyineedname <emssaoig1@gmail.com>
2026-02-11 00:22:22 +0200

    Add files via upload

--- src/dyld.rs
+++ src/dyld.rs
@@ -774,17 +774,6 @@ impl Dyld {
         Ok(function_ptr)
     }
 
-    fn objc_getClass(env: &mut Environment, name: id) -> id {
-        let class_name = env.objc.get_c_string(name);
-
-        if let Some(cls) = env.objc.lookup_class(&class_name) {
-            return cls;
-        }
-
-        // Возвращаем Nil вместо panic
-        crate::objc::nil
-        }
-
     pub fn create_guest_function(
         &mut self,
         mem: &mut Mem,
