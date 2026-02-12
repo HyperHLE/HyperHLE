d41b177b8203578d92466332ea8af9af1bdf98d5
whyineedname <emssaoig1@gmail.com>
2026-02-12 22:23:06 +0200

    Update ui_table_view_cell.rs

--- src/frameworks/uikit/ui_table_view_cell.rs
+++ src/frameworks/uikit/ui_table_view_cell.rs
@@ -4,16 +4,15 @@ use crate::frameworks::core_graphics::CGRect;
 use crate::msg;
 
 pub fn register(env: &mut Environment) {
+    // Вызываем макрос, который напрямую регистрирует классы в env
     objc_classes! {
         (env, this, _cmd);
 
         @implementation UITableViewCell : UIView
-
-        - (id)initWithFrame:(CGRect)frame reuseIdentifier:(id)identifier {
-            let obj: id = msg![env; this initWithFrame:frame];
-            obj
-        }
-
+            - (id)initWithFrame:(CGRect)frame reuseIdentifier:(id)identifier {
+                let this: id = msg![super(this); initWithFrame:frame];
+                this
+            }
         @end
-    }
+    };
 }
