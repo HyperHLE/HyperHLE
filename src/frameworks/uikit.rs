836117753c21725911215e8517937f0e79f2cd41
whyineedname <emssaoig1@gmail.com>
2026-02-12 10:59:15 +0200

    Update uikit.rs

--- src/frameworks/uikit.rs
+++ src/frameworks/uikit.rs
@@ -26,6 +26,7 @@ pub mod ui_image_picker_controller;
 pub mod ui_nib;
 pub mod ui_responder;
 pub mod ui_screen;
+pub mod ui_table_view_cell;
 pub mod ui_touch;
 pub mod ui_view;
 pub mod ui_view_controller;
