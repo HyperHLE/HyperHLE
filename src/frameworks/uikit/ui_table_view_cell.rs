a0e795e81a6838f48ec83516aed3b17ec56add51
whyineedname <emssaoig1@gmail.com>
2026-02-12 22:54:46 +0200

    Update ui_table_view_cell.rs

--- src/frameworks/uikit/ui_table_view_cell.rs
+++ src/frameworks/uikit/ui_table_view_cell.rs
@@ -4,17 +4,16 @@ use crate::frameworks::foundation::NSString;
 use crate::objc::classes::objc_classes;
 
 pub fn register(env: &mut Environment) {
-    // Создаём класс UITableViewCell, наследуемый от UIView
     objc_classes! {
         (env, this, _cmd);
 
         @implementation UITableViewCell : UIView
             // Реализация initWithFrame:reuseIdentifier:
-            - (id)initWithFrame:(CGRect)frame reuseIdentifier:(id)identifier) {
+            - (id)initWithFrame:(CGRect)frame reuseIdentifier:(id)identifier {
                 // Вызываем super initWithFrame:
                 let super_obj: id = msg_send(env, (super(this), "initWithFrame:", frame));
 
-                // Тут можно сохранить identifier, если нужно
+                // Здесь можно сохранить identifier в поле, если нужно
                 this
             }
         @end
