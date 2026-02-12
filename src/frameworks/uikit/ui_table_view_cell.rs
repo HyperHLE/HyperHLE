80ac4a99aff1d9b159c1bc191270feee4a8ac596
whyineedname <emssaoig1@gmail.com>
2026-02-12 21:49:24 +0200

    Update ui_table_view_cell.rs

--- src/frameworks/uikit/ui_table_view_cell.rs
+++ src/frameworks/uikit/ui_table_view_cell.rs
@@ -1,21 +1,19 @@
-/*
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0.
- */
-
+use crate::Environment;
 use crate::objc::{id, objc_classes};
 use crate::frameworks::core_graphics::CGRect;
 use crate::msg;
 
-objc_classes! {
-    (env, this, _cmd);
+pub fn register(env: &mut Environment) {
+    objc_classes! {
+        (env, this, _cmd);
 
-    @implementation UITableViewCell : UIView
+        @implementation UITableViewCell : UIView
 
-    - (id)initWithFrame:(CGRect)frame reuseIdentifier:(id)identifier {
-        let obj: id = msg![env; this initWithFrame:frame];
-        obj
+        - (id)initWithFrame:(CGRect)frame reuseIdentifier:(id)identifier {
+            let obj: id = msg![env; this initWithFrame:frame];
+            obj
+        }
+
+        @end
     }
-
-    @end
 }
