08e5021fce19ab21ce5e34436b61cfda2e9fb827
whyineedname <emssaoig1@gmail.com>
2026-02-12 18:19:08 +0200

    Update ui_table_view_cell.rs

--- src/frameworks/uikit/ui_table_view_cell.rs
+++ src/frameworks/uikit/ui_table_view_cell.rs
@@ -3,12 +3,30 @@
  * License, v. 2.0.
  */
 
-use crate::objc::{id, msg, msg_class};
-use crate::environment::Environment;
+use crate::objc::{
+    id, msg_send, nil,
+    objc_classes,
+};
+use crate::frameworks::uikit::ui_view::CGRect;
 
-pub fn register_table_view_cell(env: &mut Environment) {
-    env.objc.register_class("UITableViewCell", Some("UIView"), |env, this| {
-        let _self: id = msg![env; this init];
-        _self
-    });
+objc_classes! {
+    (env, this, _cmd);
+
+    @implementation UITableViewCell : UIView
+
+    - (id)initWithFrame:(CGRect)frame reuseIdentifier:(id)_reuse {
+        // super.initWithFrame:
+        let super_obj: id = msg_send(env, (this, "initWithFrame:", frame));
+        super_obj
+    }
+
+    - (id)reuseIdentifier {
+        nil
+    }
+
+    - (())prepareForReuse {
+        // intentionally empty
+    }
+
+    @end
 }
