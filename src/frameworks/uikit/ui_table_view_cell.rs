1198edc1568b7d8e3f6df7e5a08eef1e6085994f
whyineedname <emssaoig1@gmail.com>
2026-02-12 13:44:23 +0200

    Update ui_table_view_cell.rs

--- src/frameworks/uikit/ui_table_view_cell.rs
+++ src/frameworks/uikit/ui_table_view_cell.rs
@@ -3,7 +3,8 @@
  * License, v. 2.0.
  */
 
-use crate::objc::{id, msg, msg_class, Environment};
+use crate::objc::{id, msg, msg_class};
+use crate::environment::Environment;
 
 pub fn register_table_view_cell(env: &mut Environment) {
     env.objc.register_class("UITableViewCell", Some("UIView"), |env, this| {
