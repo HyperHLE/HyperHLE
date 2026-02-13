79baf1004a0f2c5c6d803a87d7d48629c4ace1a2
whyineedname <emssaoig1@gmail.com>
2026-02-13 14:36:54 +0200

    Update ui_table_view_cell.rs

--- src/frameworks/uikit/ui_table_view_cell.rs
+++ src/frameworks/uikit/ui_table_view_cell.rs
@@ -2,20 +2,19 @@
  * MPL 2.0
  */
 
-use crate::{Environment, objc_classes};
+use crate::objc_classes;
 use crate::objc::id;
 use crate::frameworks::core_graphics::CGRect;
 
-objc_classes! {
+static CLASSES: &[(&str, crate::objc::ClassTemplate)] = objc_classes! {
     (env, this, _cmd);
 
     @implementation UITableViewCell : UIView
 
     - (id)initWithFrame:(CGRect)frame reuseIdentifier:(id)reuseIdentifier {
-        // вызываем super initWithFrame:
         let this: id = msg![env; super(this) initWithFrame:frame];
         this
     }
 
     @end
-}
+};
