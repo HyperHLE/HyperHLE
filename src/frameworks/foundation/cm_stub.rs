a6d80c34f6627b993e24d6e61a42a824c6885019
whyineedname <emssaoig1@gmail.com>
2026-03-08 20:18:24 +0200

    Update cm_stub.rs

--- src/frameworks/foundation/cm_stub.rs
+++ src/frameworks/foundation/cm_stub.rs
@@ -1,23 +1,22 @@
 use crate::objc_classes;
+use crate::objc::id; // Импортируем тип 'id' напрямую
 
 pub const CLASSES: crate::objc::ClassExports = objc_classes! {
     (env, this, _cmd);
     
     @implementation CMMotionManager: NSObject
     
-    // Тот метод, который нужен игре
     - (bool)isGyroAvailable {
         false
     }
 
-    // Методы для стабильности эмулятора (чтобы NSSet не падал)
     - (u32)hash {
         0
     }
 
     - (bool)isEqual:(id)_other {
-        // Мы просто говорим, что объекты равны сами себе
-        true 
+        // Сравниваем объект сам с собой
+        this == _other
     }
 
     - (id)init {
