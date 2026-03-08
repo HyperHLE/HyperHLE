6ff671104cfef4e55d7d9e10d580e62475dff825
whyineedname <emssaoig1@gmail.com>
2026-03-08 19:55:58 +0200

    Update cm_stub.rs

--- src/frameworks/foundation/cm_stub.rs
+++ src/frameworks/foundation/cm_stub.rs
@@ -1,11 +1,27 @@
-use crate::objc_classes; // Прямой импорт макроса по совету компилятора
+use crate::objc_classes;
 
 pub const CLASSES: crate::objc::ClassExports = objc_classes! {
     (env, this, _cmd);
     
     @implementation CMMotionManager: NSObject
+    
+    // Тот метод, который нужен игре
     - (bool)isGyroAvailable {
         false
+    }
+
+    // Методы для стабильности эмулятора (чтобы NSSet не падал)
+    - (u32)hash {
+        0
+    }
+
+    - (bool)isEqual:(id)_other {
+        // Мы просто говорим, что объекты равны сами себе
+        true 
+    }
+
+    - (id)init {
+        this
     }
     @end
 };
