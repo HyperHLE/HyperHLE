c8c765af692c51428a1aa76845f1eb39b9b86007
whyineedname <emssaoig1@gmail.com>
2026-03-13 08:36:57 +0200

    Update cm_stub.rs

--- src/frameworks/foundation/cm_stub.rs
+++ src/frameworks/foundation/cm_stub.rs
@@ -1,25 +1,41 @@
 use crate::objc_classes;
-use crate::objc::id; // Импортируем тип 'id' напрямую
+use crate::objc::id;
 
 pub const CLASSES: crate::objc::ClassExports = objc_classes! {
     (env, this, _cmd);
     
     @implementation CMMotionManager: NSObject
     
+    // Главное для игры
     - (bool)isGyroAvailable {
         false
     }
 
+    // Обязательно для NSSet и коллекций
     - (u32)hash {
-        0
+        this as u32
     }
 
     - (bool)isEqual:(id)_other {
-        // Сравниваем объект сам с собой
         this == _other
     }
 
+    // Методы управления памятью (часто причина паник в touchHLE)
+    - (id)retain {
+        this
+    }
+
+    - (())release {}
+
+    - (id)autorelease {
+        this
+    }
+
     - (id)init {
+        this
+    }
+
+    - (id)description {
         this
     }
     @end
