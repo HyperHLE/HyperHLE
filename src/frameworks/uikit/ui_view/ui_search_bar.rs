279c279c8a5af93697aea7e794da455daccc1dae
whyineedname <emssaoig1@gmail.com>
2026-03-21 14:08:45 +0200

    Add files via upload

--- src/frameworks/uikit/ui_view/ui_search_bar.rs
+++ src/frameworks/uikit/ui_view/ui_search_bar.rs
@@ -6,14 +6,7 @@
 //! `UISearchBar`.
 
 use crate::frameworks::core_graphics::CGRect;
-use crate::objc::{id, msg, msg_super, nil, objc_classes, ClassExports, HostObject, NSZonePtr};
-
-pub struct UISearchBarHostObject {
-    pub delegate: id,
-    pub text: id,
-    pub placeholder: id,
-}
-impl HostObject for UISearchBarHostObject {}
+use crate::objc::{id, msg_super, nil, objc_classes, ClassExports};
 
 pub const CLASSES: ClassExports = objc_classes! {
 
@@ -21,71 +14,25 @@ pub const CLASSES: ClassExports = objc_classes! {
 
 @implementation UISearchBar: UIView
 
-+ (id)allocWithZone:(NSZonePtr)_zone {
-    let host_object = Box::new(UISearchBarHostObject {
-        delegate: nil,
-        text: nil,
-        placeholder: nil,
-    });
-    env.objc.alloc_object(this, host_object, &mut env.mem)
-}
-
 - (id)initWithFrame:(CGRect)frame {
     msg_super![env; this initWithFrame:frame]
 }
 
-- (id)delegate {
-    env.objc.borrow::<UISearchBarHostObject>(this).delegate
-}
-
-- (())setDelegate:(id)delegate {
-    env.objc.borrow_mut::<UISearchBarHostObject>(this).delegate = delegate;
-}
-
-- (id)text {
-    env.objc.borrow::<UISearchBarHostObject>(this).text
-}
-
-- (())setText:(id)text {
-    env.objc.borrow_mut::<UISearchBarHostObject>(this).text = text;
-}
-
-- (id)placeholder {
-    env.objc.borrow::<UISearchBarHostObject>(this).placeholder
-}
-
-- (())setPlaceholder:(id)placeholder {
-    env.objc.borrow_mut::<UISearchBarHostObject>(this).placeholder = placeholder;
-}
-
-- (())setBarStyle:(i32)_style {
-}
-
-- (())setShowsCancelButton:(bool)_shows {
-}
-
-- (())setShowsCancelButton:(bool)_shows animated:(bool)_animated {
-}
-
-- (())setAutocorrectionType:(i32)_type {
-}
-
-- (())setAutocapitalizationType:(i32)_type {
-}
-
-- (())setKeyboardType:(i32)_type {
-}
-
-- (())setTintColor:(id)_color {
-}
-
-- (bool)becomeFirstResponder {
-    false
-}
-
-- (bool)resignFirstResponder {
-    true
-}
+- (id)delegate { nil }
+- (())setDelegate:(id)_delegate {}
+- (id)text { nil }
+- (())setText:(id)_text {}
+- (id)placeholder { nil }
+- (())setPlaceholder:(id)_placeholder {}
+- (())setBarStyle:(i32)_style {}
+- (())setShowsCancelButton:(bool)_shows {}
+- (())setShowsCancelButton:(bool)_shows animated:(bool)_animated {}
+- (())setAutocorrectionType:(i32)_type {}
+- (())setAutocapitalizationType:(i32)_type {}
+- (())setKeyboardType:(i32)_type {}
+- (())setTintColor:(id)_color {}
+- (bool)becomeFirstResponder { false }
+- (bool)resignFirstResponder { true }
 
 @end
 
