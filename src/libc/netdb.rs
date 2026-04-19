f6aafef5ca43d9b263f295672c3eb80e759921eb
masterfanaedoodledash-cmd <masterfanaedoodledash@gmail.com>
2026-04-19 18:03:43 +0200

    Update netdb.rs

--- src/libc/netdb.rs
+++ src/libc/netdb.rs
@@ -11,7 +11,6 @@ use crate::export_c_func;
 use crate::libc::sys::socket::{sockaddr, AF_INET, SOCK_DGRAM, SOCK_STREAM};
 use crate::mem::{guest_size_of, ConstPtr, MutPtr, MutVoidPtr, Ptr, SafeRead};
 use crate::Environment;
-use libc::hostent;
 use std::ops::Add;
 
 const AI_PASSIVE:    i32 = 0x1;
@@ -76,6 +75,13 @@ struct hostent_guest {
     h_addr_list: MutPtr<MutPtr<u8>>, // NULL-terminated address list
 }
 unsafe impl SafeRead for hostent_guest {}
+
+#[derive(Copy, Clone, Debug)]
+#[repr(C, packed)]
+struct hostent {
+    _private: [u8; 0],
+}
+unsafe impl SafeRead for hostent {}
 
 /// `struct servent` layout.
 #[derive(Copy, Clone, Debug)]
