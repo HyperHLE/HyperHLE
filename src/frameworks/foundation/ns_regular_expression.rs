6460ad3a2a72ee31c3c183ffd02af0a7b9b4670e
JamesHeyman0993 <gamingbeast573337@gmail.com>
2026-04-26 15:24:04 -0400

    Fix formatting and syntax in ns_regular_expression.rs

--- src/frameworks/foundation/ns_regular_expression.rs
+++ src/frameworks/foundation/ns_regular_expression.rs
@@ -8,19 +8,20 @@ pub const CLASSES: ClassExports = objc_classes! {
     @implementation NSRegularExpression: NSObject
 
     + (id)allocWithZone:(NSZonePtr)_zone {
-        // Open the NullableBox to access the ObjC engine
         let instance = env.objc.as_mut().unwrap().alloc_instance(this);
         log!("NSRegularExpression: Created instance {:?}.", instance);
         instance
-    };
+    }
 
     - (id)initWithPattern:(id)_pattern options:(u64)_options error:(id)_error {
         log!("NSRegularExpression: initWithPattern stubbed.");
         this
-    };
+    }
 
     - (id)matchesInString:(id)_string options:(u64)_options range:(crate::frameworks::foundation::NSRange)_range {
         log!("NSRegularExpression: matchesInString returning nil.");
         crate::objc::nil
-    };
+    }
+
+    @end
 };
