d4d8b37347cfd1dbd85a7ff15327b32e7405bb3c
JamesHeyman0993 <gamingbeast573337@gmail.com>
2026-04-26 11:03:20 -0400

    Update ns_regular_expression.rs

--- src/frameworks/foundation/ns_regular_expression.rs
+++ src/frameworks/foundation/ns_regular_expression.rs
@@ -4,10 +4,10 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-use crate::objc::{Id, Sel, Class, ClassExports, ClassMethod};
+use crate::objc::{Id, Sel, ClassExports, ClassMethod};
 use crate::log;
+use crate::Environment;
 
-/// This is what foundation.rs needs to see to actually "load" the class.
 pub const CLASSES: ClassExports = &[
     ClassMethod {
         class_name: "NSRegularExpression",
@@ -16,29 +16,25 @@ pub const CLASSES: ClassExports = &[
     },
 ];
 
-fn add_methods(class: &mut Class) {
-    // + (id)alloc
-    class.add_class_method(sel!(alloc), alloc as _);
-    
-    // - (id)initWithPattern:options:error:
-    class.add_method(sel!(initWithPattern:options:error:), init_with_pattern as _);
-    
-    // - (id)matchesInString:options:range:
-    class.add_method(sel!(matchesInString:options:range:), matches_in_string as _);
+/// The manual registration style for your specific touchHLE build
+fn add_methods(env: &mut Environment, class: Id) {
+    crate::objc::add_class_method(env, class, sel!(alloc), alloc as _);
+    crate::objc::add_method(env, class, sel!(initWithPattern:options:error:), init_with_pattern as _);
+    crate::objc::add_method(env, class, sel!(matchesInString:options:range:), matches_in_string as _);
 }
 
-extern "C" fn alloc(class: &Class, _sel: Sel) -> Id {
-    let instance = class.create_instance();
+extern "C" fn alloc(env: &mut Environment, class: Id, _sel: Sel) -> Id {
+    let instance = crate::objc::alloc_instance(env, class);
     log!("NSRegularExpression: Created stub instance {:?}.", instance);
     instance
 }
 
-extern "C" fn init_with_pattern(this: Id, _sel: Sel, _pattern: Id, _options: u64, _error: Id) -> Id {
-    log!("NSRegularExpression: initWithPattern called (Stubbing success).");
+extern "C" fn init_with_pattern(_env: &mut Environment, this: Id, _sel: Sel, _pattern: Id, _options: u64, _error: Id) -> Id {
+    log!("NSRegularExpression: initWithPattern called.");
     this
 }
 
-extern "C" fn matches_in_string(_this: Id, _sel: Sel, _string: Id, _options: u64, _range: [usize; 2]) -> Id {
-    log!("NSRegularExpression: matchesInString called. Returning empty array.");
-    crate::frameworks::foundation::ns_array::empty_array()
+extern "C" fn matches_in_string(env: &mut Environment, _this: Id, _sel: Sel, _string: Id, _options: u64, _range: [usize; 2]) -> Id {
+    log!("NSRegularExpression: matchesInString returning empty array.");
+    crate::frameworks::foundation::ns_array::empty_array(env)
 }
