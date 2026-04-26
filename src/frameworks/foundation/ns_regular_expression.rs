7ed37e6e02a5c681736e5d52cb00e410caeaa691
JamesHeyman0993 <gamingbeast573337@gmail.com>
2026-04-26 16:32:50 -0400

    Update ns_regular_expression.rs

--- src/frameworks/foundation/ns_regular_expression.rs
+++ src/frameworks/foundation/ns_regular_expression.rs
@@ -7,13 +7,13 @@ pub const CLASSES: ClassExports = objc_classes! {
 
     @implementation NSRegularExpression: NSObject
 
-    + (id)allocWithZone:(NSZonePtr)_zone {
-        // Explicitly dereference the NullableBox to reach alloc_instance
+        + (id)allocWithZone:(NSZonePtr)_zone {
+        // Use (*env.objc) to explicitly trigger DerefMut and find alloc_instance
         let instance = (*env.objc).alloc_instance(this, &mut env.mem);
         log!("NSRegularExpression: Created instance {:?}.", instance);
         instance
-    }
-
+        }
+    
     - (id)initWithPattern:(id)_pattern options:(u64)_options error:(id)_error {
         // CRITICAL: We must notify the engine that this instance is being initialized
         (*env.objc).init_instance(this);
