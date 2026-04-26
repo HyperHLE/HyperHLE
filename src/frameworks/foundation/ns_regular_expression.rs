0d05fccb5851e8f51ad27b1c8a8e3f621efe2b75
JamesHeyman0993 <gamingbeast573337@gmail.com>
2026-04-26 17:46:24 -0400

    Update ns_regular_expression.rs

--- src/frameworks/foundation/ns_regular_expression.rs
+++ src/frameworks/foundation/ns_regular_expression.rs
@@ -1,31 +1,34 @@
-use crate::objc::{id, SEL, ClassExports, objc_classes, NSZonePtr};
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+use crate::objc::{id as Id, SEL as Sel, ClassExports}; // Matches your project's naming
 use crate::log;
-use crate::Environment;
+use crate::frameworks::foundation::NSRange;
 
-pub const CLASSES: ClassExports = objc_classes! {
-    (env, this, _cmd);
+// If your project uses the manual "add_methods" style, keep this.
+// If it uses the macro style, this file needs to be listed in foundation/mod.rs
+pub fn add_methods(class: &mut crate::objc::Class) {
+    class.add_class_method(sel!(alloc), alloc as _);
+    class.add_method(sel!(initWithPattern:options:error:), init_with_pattern as _);
+    class.add_method(sel!(matchesInString:options:range:), matches_in_string as _);
+}
 
-    @implementation NSRegularExpression: NSObject
+extern "C" fn alloc(class: &crate::objc::Class, _sel: Sel) -> Id {
+    let instance = class.create_instance();
+    log!("NSRegularExpression: Created stub instance {:?}.", instance);
+    instance
+}
 
-            + (id)allocWithZone:(NSZonePtr)_zone {
-        // Use alloc_object with a dummy host object ()
-        let instance = (*env.objc).alloc_object(this, (), &mut env.mem);
-        log!("NSRegularExpression: Created instance {:?}.", instance);
-        instance
-    }
+extern "C" fn init_with_pattern(this: Id, _sel: Sel, _pattern: Id, _options: u64, _error: Id) -> Id {
+    log!("NSRegularExpression: initWithPattern called.");
+    this
+}
 
-    - (id)initWithPattern:(id)_pattern options:(u64)_options error:(id)_error {
-        // If init_instance doesn't exist, it might be init_object or 
-        // simply not required if alloc_object handles registration.
-        // Try removing the init_instance call if it keeps failing.
-        log!("NSRegularExpression: initWithPattern initialized {:?}.", this);
-        this
-    }
-    
-    - (id)matchesInString:(id)_string options:(u64)_options range:(crate::frameworks::foundation::NSRange)_range {
-        log!("NSRegularExpression: matchesInString returning nil.");
-        crate::objc::nil
-    }
-
-    @end
-};
+extern "C" fn matches_in_string(_this: Id, _sel: Sel, _string: Id, _options: u64, _range: NSRange) -> Id {
+    log!("NSRegularExpression: matchesInString called. Returning empty array.");
+    // This assumes ns_array has a helper. If this fails, use crate::objc::nil
+    crate::objc::nil 
+}
