d790b4701c54b614a959d6d6bcc4ed1c31354f20
JamesHeyman0993 <gamingbeast573337@gmail.com>
2026-04-26 17:59:22 -0400

    Update ns_regular_expression.rs

--- src/frameworks/foundation/ns_regular_expression.rs
+++ src/frameworks/foundation/ns_regular_expression.rs
@@ -4,31 +4,32 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-use crate::objc::{id as Id, SEL as Sel, ClassExports}; // Matches your project's naming
+use crate::objc::{id, SEL, ClassExports, objc_classes, NSZonePtr};
 use crate::log;
+use crate::Environment;
 use crate::frameworks::foundation::NSRange;
 
-// If your project uses the manual "add_methods" style, keep this.
-// If it uses the macro style, this file needs to be listed in foundation/mod.rs
-pub fn add_methods(class: &mut crate::objc::Class) {
-    class.add_class_method(sel!(alloc), alloc as _);
-    class.add_method(sel!(initWithPattern:options:error:), init_with_pattern as _);
-    class.add_method(sel!(matchesInString:options:range:), matches_in_string as _);
-}
+pub const CLASSES: ClassExports = objc_classes! {
+    (env, this, _cmd);
 
-extern "C" fn alloc(class: &crate::objc::Class, _sel: Sel) -> Id {
-    let instance = class.create_instance();
-    log!("NSRegularExpression: Created stub instance {:?}.", instance);
-    instance
-}
+    @implementation NSRegularExpression: NSObject
 
-extern "C" fn init_with_pattern(this: Id, _sel: Sel, _pattern: Id, _options: u64, _error: Id) -> Id {
-    log!("NSRegularExpression: initWithPattern called.");
-    this
-}
+    + (id)allocWithZone:(NSZonePtr)_zone {
+        // This is the macro equivalent of class.create_instance()
+        let instance = (*env.objc).alloc_object(this, (), &mut env.mem);
+        log!("NSRegularExpression: Created instance {:?}.", instance);
+        instance
+    }
 
-extern "C" fn matches_in_string(_this: Id, _sel: Sel, _string: Id, _options: u64, _range: NSRange) -> Id {
-    log!("NSRegularExpression: matchesInString called. Returning empty array.");
-    // This assumes ns_array has a helper. If this fails, use crate::objc::nil
-    crate::objc::nil 
-}
+    - (id)initWithPattern:(id)_pattern options:(u64)_options error:(id)_error {
+        log!("NSRegularExpression: initWithPattern called.");
+        this
+    }
+
+    - (id)matchesInString:(id)_string options:(u64)_options range:(NSRange)_range {
+        log!("NSRegularExpression: matchesInString called. Returning nil.");
+        crate::objc::nil
+    }
+
+    @end
+};
