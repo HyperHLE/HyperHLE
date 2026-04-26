c4512b033798e26b610a4e2a9ffe93cd19632eab
JamesHeyman0993 <gamingbeast573337@gmail.com>
2026-04-26 11:14:55 -0400

    Refactor NSRegularExpression to use new objc syntax

--- src/frameworks/foundation/ns_regular_expression.rs
+++ src/frameworks/foundation/ns_regular_expression.rs
@@ -4,37 +4,38 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-use crate::objc::{Id, Sel, ClassExports, ClassMethod};
+use crate::objc::{id, SEL, ClassExports, ExportedClass};
 use crate::log;
 use crate::Environment;
 
 pub const CLASSES: ClassExports = &[
-    ClassMethod {
+    ExportedClass {
         class_name: "NSRegularExpression",
         parent_class_name: "NSObject",
         add_methods,
     },
 ];
 
-/// The manual registration style for your specific touchHLE build
-fn add_methods(env: &mut Environment, class: Id) {
-    crate::objc::add_class_method(env, class, sel!(alloc), alloc as _);
-    crate::objc::add_method(env, class, sel!(initWithPattern:options:error:), init_with_pattern as _);
-    crate::objc::add_method(env, class, sel!(matchesInString:options:range:), matches_in_string as _);
+fn add_methods(env: &mut Environment, class: id) {
+    // In your build, we use the env.objc methods directly
+    env.objc.add_class_method(class, selector!("alloc"), alloc as _);
+    env.objc.add_method(class, selector!("initWithPattern:options:error:"), init_with_pattern as _);
+    env.objc.add_method(class, selector!("matchesInString:options:range:"), matches_in_string as _);
 }
 
-extern "C" fn alloc(env: &mut Environment, class: Id, _sel: Sel) -> Id {
-    let instance = crate::objc::alloc_instance(env, class);
+extern "C" fn alloc(env: &mut Environment, class: id, _sel: SEL) -> id {
+    let instance = env.objc.alloc_instance(class);
     log!("NSRegularExpression: Created stub instance {:?}.", instance);
     instance
 }
 
-extern "C" fn init_with_pattern(_env: &mut Environment, this: Id, _sel: Sel, _pattern: Id, _options: u64, _error: Id) -> Id {
+extern "C" fn init_with_pattern(_env: &mut Environment, this: id, _sel: SEL, _pattern: id, _options: u64, _error: id) -> id {
     log!("NSRegularExpression: initWithPattern called.");
     this
 }
 
-extern "C" fn matches_in_string(env: &mut Environment, _this: Id, _sel: Sel, _string: Id, _options: u64, _range: [usize; 2]) -> Id {
+extern "C" fn matches_in_string(env: &mut Environment, _this: id, _sel: SEL, _string: id, _options: u64, _range: [usize; 2]) -> id {
     log!("NSRegularExpression: matchesInString returning empty array.");
-    crate::frameworks::foundation::ns_array::empty_array(env)
+    // We'll use the common way to get an empty array in your build:
+    crate::frameworks::foundation::ns_array::new(env)
 }
