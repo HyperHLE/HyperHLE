75bdd6493ddc04020bcbfa2bd2efe34515a1d87a
JamesHeyman0993 <gamingbeast573337@gmail.com>
2026-04-26 14:43:37 -0400

    Update ns_regular_expression.rs

--- src/frameworks/foundation/ns_regular_expression.rs
+++ src/frameworks/foundation/ns_regular_expression.rs
@@ -8,20 +8,19 @@ pub const CLASSES: ClassExports = objc_classes! {
     @implementation NSRegularExpression: NSObject
 
     + (id)allocWithZone:(NSZonePtr)_zone {
-        let instance = env.objc.alloc_instance(this);
+        // Open the NullableBox to access the ObjC engine
+        let instance = env.objc.as_mut().unwrap().alloc_instance(this);
         log!("NSRegularExpression: Created instance {:?}.", instance);
         instance
-    }
+    };
 
     - (id)initWithPattern:(id)_pattern options:(u64)_options error:(id)_error {
         log!("NSRegularExpression: initWithPattern stubbed.");
         this
-    }
+    };
 
     - (id)matchesInString:(id)_string options:(u64)_options range:(crate::frameworks::foundation::NSRange)_range {
         log!("NSRegularExpression: matchesInString returning nil.");
-        crate::objc::id::null()
-    }
-
-    @end
+        crate::objc::nil
+    };
 };
