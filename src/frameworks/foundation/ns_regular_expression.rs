6c672d2dbc0ab92340a7b1ed59aa1d0002e36d2b
JamesHeyman0993 <gamingbeast573337@gmail.com>
2026-04-26 14:20:34 -0400

    Update ns_regular_expression.rs

--- src/frameworks/foundation/ns_regular_expression.rs
+++ src/frameworks/foundation/ns_regular_expression.rs
@@ -1,35 +1,25 @@
-use crate::objc::{id, SEL, ClassExports, ClassTemplate};
-use crate::selector;
+use crate::objc::{id, SEL, ClassExports, objc_classes, NSZonePtr};
 use crate::log;
 use crate::Environment;
 
-pub const CLASSES: ClassExports = &[
-    ("NSRegularExpression", ClassTemplate {
-        name: "NSRegularExpression",
-        superclass: Some("NSObject"),
-        class_methods: &[
-            (selector!(_; alloc), alloc as _),
-        ],
-        instance_methods: &[
-            (selector!(_; initWithPattern, options, error), init_with_pattern as _),
-            (selector!(_; matchesInString, options, range), matches_in_string as _),
-        ],
-    }),
-];
+pub const CLASSES: ClassExports = objc_classes! {
+    (env, this, _cmd);
 
-extern "C" fn alloc(env: &mut Environment, class: id, _sel: SEL) -> id {
-    // We use .as_mut().unwrap() to get past the NullableBox
-    let instance = env.objc.as_mut().unwrap().alloc_instance(class);
-    log!("NSRegularExpression: Created instance {:?}.", instance);
-    instance
-}
+    @implementation NSRegularExpression: NSObject
 
-extern "C" fn init_with_pattern(_env: &mut Environment, this: id, _sel: SEL, _pattern: id, _options: u64, _error: id) -> id {
-    log!("NSRegularExpression: initWithPattern stubbed.");
-    this
-}
+    + (id)allocWithZone:(NSZonePtr)_zone {
+        let instance = env.objc.alloc_instance(this);
+        log!("NSRegularExpression: Created instance {:?}.", instance);
+        instance
+    }
 
-extern "C" fn matches_in_string(_env: &mut Environment, _this: id, _sel: SEL, _string: id, _options: u64, _loc: u64, _len: u64) -> id {
-    log!("NSRegularExpression: matchesInString returning nil.");
-    crate::objc::id::null()
-}
+    - (id)initWithPattern:(id)_pattern options:(u64)_options error:(id)_error {
+        log!("NSRegularExpression: initWithPattern stubbed.");
+        this
+    }
+
+    - (id)matchesInString:(id)_string options:(u64)_options range:(crate::frameworks::foundation::NSRange)_range {
+        log!("NSRegularExpression: matchesInString returning nil.");
+        crate::objc::id::null()
+    }
+};
