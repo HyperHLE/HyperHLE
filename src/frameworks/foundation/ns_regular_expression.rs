b087c11abf3c93bf093b74c8834e9f2ac27a5e86
JamesHeyman0993 <gamingbeast573337@gmail.com>
2026-04-26 16:39:55 -0400

    Update ns_regular_expression.rs

--- src/frameworks/foundation/ns_regular_expression.rs
+++ src/frameworks/foundation/ns_regular_expression.rs
@@ -7,20 +7,21 @@ pub const CLASSES: ClassExports = objc_classes! {
 
     @implementation NSRegularExpression: NSObject
 
-        + (id)allocWithZone:(NSZonePtr)_zone {
-        // Use (*env.objc) to explicitly trigger DerefMut and find alloc_instance
-        let instance = (*env.objc).alloc_instance(this, &mut env.mem);
+            + (id)allocWithZone:(NSZonePtr)_zone {
+        // Use alloc_object with a dummy host object ()
+        let instance = (*env.objc).alloc_object(this, (), &mut env.mem);
         log!("NSRegularExpression: Created instance {:?}.", instance);
         instance
-        }
-    
+    }
+
     - (id)initWithPattern:(id)_pattern options:(u64)_options error:(id)_error {
-        // CRITICAL: We must notify the engine that this instance is being initialized
-        (*env.objc).init_instance(this);
+        // If init_instance doesn't exist, it might be init_object or 
+        // simply not required if alloc_object handles registration.
+        // Try removing the init_instance call if it keeps failing.
         log!("NSRegularExpression: initWithPattern initialized {:?}.", this);
         this
     }
-
+    
     - (id)matchesInString:(id)_string options:(u64)_options range:(crate::frameworks::foundation::NSRange)_range {
         log!("NSRegularExpression: matchesInString returning nil.");
         crate::objc::nil
