4149e2bd031ceca6011281281da9ab0ad84afd69
JamesHeyman0993 <gamingbeast573337@gmail.com>
2026-04-26 12:20:38 -0400

    Update ns_regular_expression.rs

--- src/frameworks/foundation/ns_regular_expression.rs
+++ src/frameworks/foundation/ns_regular_expression.rs
@@ -5,7 +5,6 @@
  */
 
 use crate::objc::{id, SEL, ClassExports, ClassTemplate};
-use crate::selector; 
 use crate::log;
 use crate::Environment;
 
@@ -14,27 +13,28 @@ pub const CLASSES: ClassExports = &[
         name: "NSRegularExpression",
         superclass: "NSObject",
         class_methods: &[
-            (selector!(alloc), alloc as _),
+            (selector!("alloc"), alloc as _),
         ],
         instance_methods: &[
-            (selector!(initWithPattern, options, error), init_with_pattern as _),
-            (selector!(matchesInString, options, range), matches_in_string as _),
+            (selector!("initWithPattern:options:error:"), init_with_pattern as _),
+            (selector!("matchesInString:options:range:"), matches_in_string as _),
         ],
     }),
 ];
 
 extern "C" fn alloc(env: &mut Environment, class: id, _sel: SEL) -> id {
+    // Accessing the ObjC engine from the NullableBox
     let instance = env.objc.as_mut().unwrap().alloc_instance(class);
     log!("NSRegularExpression: Created stub instance {:?}.", instance);
     instance
 }
 
 extern "C" fn init_with_pattern(_env: &mut Environment, this: id, _sel: SEL, _pattern: id, _options: u64, _error: id) -> id {
-    log!("NSRegularExpression: initWithPattern called.");
+    log!("NSRegularExpression: initWithPattern stub called.");
     this
 }
 
 extern "C" fn matches_in_string(_env: &mut Environment, _this: id, _sel: SEL, _string: id, _options: u64, _range: crate::frameworks::foundation::NSRange) -> id {
     log!("NSRegularExpression: matchesInString returning nil.");
-    id::null() // This replaces the "0" error
+    id::null()
 }
